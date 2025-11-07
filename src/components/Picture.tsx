f66540be03dbd8a12d84ebd26a43950dd5831f2c
Ivan Dalmet <ivan@dalmet.fr>
2025-11-07 13:34:09 +0100

    feat: cursor love

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -7,9 +7,15 @@ import hallowenHatSrc from "@/../public/halloween-hat.png";
 import { useState } from "react";
 import { useActiveTheme } from "@/lib/theme";
 import { useStoreAchievements } from "@/components/Achivements/store";
+import {
+  Cursor,
+  CursorFollow,
+  CursorProvider,
+} from "@/components/ui/animated-cursor";
 
 export const Picture = () => {
   const { theme } = useActiveTheme();
+  const achivements = useStoreAchievements((s) => s.achivements);
   const triggerAchievement = useStoreAchievements((s) => s.triggerAchievement);
   const [isLoaded, setIsLoaded] = useState(false);
   return (
@@ -32,6 +38,27 @@ export const Picture = () => {
           alt=""
           className="pointer-events-none absolute left-[-2%] top-[-22%] z-10 aspect-square w-[110%] max-w-none"
         />
+      )}
+      {theme?.name === "romantic-love" && (
+        <CursorProvider>
+          <Cursor>
+            <svg className="size-6 text-[pink]" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                stroke="rgba(0,0,0,0.6)"
+                strokeWidth={2}
+                d="m12 19.654l-.758-.685q-2.448-2.236-4.05-3.828q-1.601-1.593-2.528-2.81t-1.296-2.2T3 8.15q0-1.908 1.296-3.204T7.5 3.65q1.32 0 2.475.675T12 6.289Q12.87 5 14.025 4.325T16.5 3.65q1.908 0 3.204 1.296T21 8.15q0 .996-.368 1.98q-.369.986-1.296 2.202t-2.519 2.809q-1.592 1.592-4.06 3.828z"
+              />
+            </svg>
+          </Cursor>
+          {!achivements.love?.status && (
+            <CursorFollow>
+              <div className="whitespace-nowrap rounded-lg bg-highlight px-2 py-1 text-sm text-background shadow-lg">
+                Say "Hi" 💋
+              </div>
+            </CursorFollow>
+          )}
+        </CursorProvider>
       )}
       <Image
         className="relative"
